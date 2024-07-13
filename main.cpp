a48e1573e8cc4cbb6c1d1bf28f85f32a67678840
Dax <u20221c794@upc.edu.pe>
2024-07-13 17:45:03 +0000

    proyecto 1 definicion de pines

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "mbed.h"
+#include ""
+DigitalIn Button(D2);
+
+AnalogIn sensor1(A0);
+AnalogIn sensor2(A1);
+
+DigitalOut alarmBuzzer(D1);
+DigitalOut alarmLed(D0);
+
+bool alarmSate = OFF;
+
+void inputsInit()
+{
+    alarmBuzzer.mode(PullDown);
+
+}
 
 int main()
 {
-    DigitalIn B1_USER(BUTTON1);
-
-    DigitalOut LD1(LED1);
-
-    while (true) {
-        LD1 = B1_USER;
-    }
 }
